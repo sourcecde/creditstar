04b30a504e8b950ef8c0ebd8c6ede47a78c83abf
sourcecde <dbrj.prof@gmail.com>
2019-09-13 10:23:06 +0530

    junk code removed

--- models/LoanSearch.php
+++ models/LoanSearch.php
@@ -55,7 +55,6 @@ class LoanSearch extends Loan
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
             return $dataProvider;
         }
 
